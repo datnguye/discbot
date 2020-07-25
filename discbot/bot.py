bce3833261c4202d765f8eab57fc0486cbe6c078
datnguye <datnguyen.it09@gmail.com>
2020-07-25 22:03:44 +0700

    updates

--- discbot/bot.py
+++ discbot/bot.py
@@ -8,20 +8,19 @@ from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
 ERRORPATH = os.getenv('ERROR_PATH')
+GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='-')
 
 @bot.event
 async def on_ready():
-    guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
-    print(f'{bot.user} is connected to the following guild: {guild.name} (id: {guild.id})')
-    
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+    print(f'{bot.user} is ready on:')
+    for guild in bot.guilds:
+        print(f'    {guild.name} ({guild.id})')
+    bot.remove_command('help')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('--help or --h'))
 
-    
 @bot.event
 async def on_member_join(member):
     guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
@@ -32,43 +31,47 @@ async def on_member_join(member):
 async def on_error(event, *args, **kwargs):
     timestamp = datetime.today().strftime('%Y%m%d')
     with open(ERRORPATH.replace("<time>", timestamp), 'a') as f:
-        if event == 'on_message':
-            f.write(f'Unhandled message: {args[0]}\n')
-        else:
-            raise
+        f.write(f'Unhandled message: {args[0]}\n')
 
-@bot.command(name='99', help='Giving Brooklyn 99 quotes')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-
-@bot.command(name='roll', help='Simulates rolling dice.')
+@bot.command(name='roll', aliases=['r','R'], description='Simulates rolling dice.')
 async def roll(ctx, number_of_dice=3, number_of_sides=100):
-    #print(ctx.author.id)
     dice = [
         str(random.choice(range(1, number_of_sides + 1)))
         for _ in range(number_of_dice)
     ]
     await ctx.send(','.join(dice))
 
-@bot.command(name='check', help='MOD: Check rights')
-@commands.has_any_role('admin')
-async def check(ctx):
-    await ctx.send('You are cool indeed')
+@bot.command(name="-help", aliases=['-h'], description="Returns all commands available")
+async def help(ctx):
+    helptext = "```"
+    for command in bot.commands:
+        command_name = command.__original_kwargs__['name']
+        if command_name == '-help':
+            continue
+        
+        if command_name in ['quit'] and ctx.message.author.guild_permissions.administrator:
+            helptext += f"-{command_name}: {command.__original_kwargs__['description']}\n"
+        else:
+            helptext += f"-{command_name}: {command.__original_kwargs__['description']}\n"
 
-@bot.command(name='quit', help='Quit and log out the bot. Affected by owner request only')
+    helptext += "```"
+    await ctx.send(helptext)
+
+# @bot.command(name='check', description='ADMIN: Check rights')
+# @commands.has_permissions(administrator=True)
+# async def check(ctx):
+#     await ctx.send('You are cool indeed')
+
+@bot.command(name='quit', aliases=['q','Q'], description='OWNER - Quit and log out the bot. Affected by owner request only')
 @commands.is_owner()
 async def shutdown(ctx):
-    await ctx.send('OK! Logged out.')
+    await ctx.send('Roger that! Logged out.')
     await ctx.bot.logout()
+
+@bot.command(name='raise', aliases=['rs','Rs', 'RS'], description='OWNER - Raise exceptions')
+@commands.is_owner()
+async def raise_exception(ctx):
+    raise
 
 def start():
     bot.run(TOKEN)
